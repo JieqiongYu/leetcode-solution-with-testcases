d206934666c589a72457f443353bd6f5686b1942
Jieqiong Yu <yjieqiong@gmail.com>
2018-08-08 14:25:20 +0800

    Add LeetCode 844 Backspace String Compare

---
+++ src/test/java/Stack/BackspaceStringCompareTest.java
@@ -0,0 +1,84 @@
+package Stack;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+
+/**
+ * Test for [LeetCode][844] Backspace String Compare
+ *
+ * @author jieqiong.yu
+ */
+public class BackspaceStringCompareTest {
+
+  @Mock
+  private BackspaceStringCompare solution;
+
+  public BackspaceStringCompareTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+    solution = new BackspaceStringCompare();
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  /**
+   * Example 1
+   */
+  @Test
+  public void testcase1() {
+    String s = "ab#c";
+    String T = "ad#c";
+    boolean expected = true;
+    assertEquals(expected, solution.backspaceCompare(s, T));
+  }
+
+  /**
+   * Example 2
+   */
+  @Test
+  public void testcase2() {
+    String s = "ab##";
+    String T = "c#d#";
+    boolean expected = true;
+    assertEquals(expected, solution.backspaceCompare(s, T));
+  }
+
+  /**
+   * Example 3
+   */
+  @Test
+  public void testcase3() {
+    String s = "a##c";
+    String T = "#a#c";
+    boolean expected = true;
+    assertEquals(expected, solution.backspaceCompare(s, T));
+  }
+
+  /**
+   * Example 4
+   */
+  @Test
+  public void testcase4() {
+    String s = "a#c";
+    String T = "b";
+    boolean expected = false;
+    assertEquals(expected, solution.backspaceCompare(s, T));
+  }
+}
