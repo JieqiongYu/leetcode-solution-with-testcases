708017498e6cf7b606d3132fc015f6869389cd8d
Jieqiong Yu <jieqiong-yu@mercari.com>
2020-07-06 22:29:38 +0900

    937. Reorder Data in Log Files

---
+++ src/test/java/String/ReorderDataInLogFilesTest.java
@@ -0,0 +1,49 @@
+package String;
+
+import org.junit.*;
+import org.mockito.Mock;
+
+public class ReorderDataInLogFilesTest {
+    @Mock
+    private ReorderDataInLogFiles solution;
+
+    public ReorderDataInLogFilesTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        solution = new ReorderDataInLogFiles();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Example 1
+     */
+    @Test
+    public void testcase1() {
+        String[] logs = new String[]{"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"};
+        String[] expected = new String[]{"let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"};
+        Assert.assertArrayEquals(expected, solution.reorderLogFiles(logs));
+    }
+
+    /**
+     * Example 2
+     */
+    @Test
+    public void testcase2() {
+        String[] logs = new String[]{"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo", "a2 act car"};
+        String[] expected = new String[]{"a2 act car", "g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"};
+        Assert.assertArrayEquals(expected, solution.reorderLogFiles(logs));
+    }
+}
