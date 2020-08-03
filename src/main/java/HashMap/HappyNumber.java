06eb3dae8ca5b1f6607d40cd83d50a614c4e11c7
Jieqiong Yu <45551663+Jieqiong-Mer@users.noreply.github.com>
2020-08-03 22:23:15 +0900

    Happy number 202 (#22)
    * 136. Single Number April 1
    
    * 202. Happy Number April 2
    
    Co-authored-by: Mer <yjieqiong@gmail.com>

---
+++ src/main/java/HashMap/HappyNumber.java
@@ -0,0 +1,31 @@
+package HashMap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1) {
+            if (set.contains(n)) {
+                return false;
+            }
+
+            set.add(n);
+            n = getNextHappy(n);
+        }
+
+        return true;
+    }
+
+    private int getNextHappy(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
+
