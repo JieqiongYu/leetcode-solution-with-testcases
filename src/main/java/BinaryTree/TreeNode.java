f850f33dd3a5a33b492e80d3bca5c34a3c695b47
Jieqiong-Mer <jieqiong-yu@mercari.com>
2020-10-06 20:13:41 +0900

    [701. Insert into a binary search tree]

--- src/main/java/BinaryTree/TreeNode.java
+++ src/main/java/BinaryTree/TreeNode.java
@@ -1,20 +1,33 @@
 package BinaryTree;
 
+import java.util.Objects;
+
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TreeNode)) {
+            return false;
+        }
+
+        TreeNode treeNode = (TreeNode) o;
+
+        if (val != treeNode.val) {
+            return false;
+        }
+        if (!Objects.equals(left, treeNode.left)) {
+            return false;
+        }
+        return Objects.equals(right, treeNode.right);
     }
 }
