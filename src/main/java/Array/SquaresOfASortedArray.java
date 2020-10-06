7637a2088a59e2db6869894276b4d189b0fbc929
Jieqiong-Mer <jieqiong-yu@mercari.com>
2020-10-06 22:58:51 +0900

    [977 - Squares of a Sorted Array]

---
+++ src/main/java/Array/SquaresOfASortedArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+    public int[] sortedSquares(int[] A) {
+        int[] squares = new int[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+            squares[i] = A[i] * A[i];
+        }
+
+        Arrays.sort(squares);
+
+        return squares;
+    }
+}
