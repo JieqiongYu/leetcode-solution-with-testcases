5fda8282cc9825755abc5024d71707ba0424704f
Jieqiong Yu <45551663+Jieqiong-Mer@users.noreply.github.com>
2020-11-10 21:29:24 +0900

    1480. Running Sum of 1d Array (#56)

---
+++ src/main/java/Array/RunningSumOf1DArray.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class RunningSumOf1DArray {
+    public int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = nums[i] + res[i - 1];
+        }
+
+        return res;
+    }
+}
